fb0dc9906d8876ccf92fd6be8be738a4dcb251cf
Lucky <vagyasri2011@gmail.com>
2021-05-13 17:47:21 +0200

    Define my_select

--- enum.rb
+++ enum.rb
@@ -17,6 +17,24 @@ module Enumerable
             j += 1
         end 
     end   
-end
+
 
    #%w[Sharon Leo Leila Brian Arun].my_each_with_index { |friend, index| puts "#{index}: #{friend}" if index.odd? }
+
+    def my_select
+        return enum_for unless block_given?
+        my_each do |i|
+            self.delete(i) if !yield i  
+        end
+        self
+    end
+end
+   
+=begin
+    For Array
+    puts %w[Sharon Leo Leila Brian Arun].my_select { |friend| friend != 'Brian' }
+
+    For Hash
+    c = {"a" => 100, "c" => 300, "b" => 200}
+    puts c.select {|key, value| key < "b"}
+=end   
